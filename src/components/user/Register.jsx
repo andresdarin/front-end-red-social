060177b341c0e2f3f0b0d574d090e27acfdfa9a6
Andrés Darín <andres.darin05@gmail.com>
2024-08-07 08:44:35 -0300

    Register

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -8,7 +8,29 @@ export const Register = () => {
             </header>
 
             <div class="content__posts">
-
+                <form className='register-form'>
+                    <div className="form-group">
+                        <label htmlFor="name">Nombre</label>
+                        <input type="text" name='name' />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="surname">Apellido</label>
+                        <input type="text" name='surname' />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="nick">Nick</label>
+                        <input type="text" name='nick' />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">eMail</label>
+                        <input type="text" name='email' />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Contraseña</label>
+                        <input type="text" name='password' />
+                    </div>
+                    <input type='submit' value='Registrate' className='btn btn-success'></input>
+                </form>
 
             </div>
         </>
