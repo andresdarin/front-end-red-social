c218ff7ff308258eae512f3be33c4ffff94229af
Andrés Darín <andres.darin05@gmail.com>
2024-08-07 10:09:47 -0300

    Login | FormLogin

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
+import { useForm } from '../../hooks/useForm'
 
 export const Login = () => {
+
+    const { form, changed } = useForm({})
+    const loginUser = async (e) => {
+        e.preventDefault();
+
+        //Datos del formulario
+        let userToLogin = form;
+
+        //Peticion al backend 
+
+        //Persistir los datos en el navegador
+
+    }
+
     return (
         <>
-            <header class="content__header content__header--public">
-                <h1 class="content__title">Login</h1>
+            <header className="content__header content__header--public">
+                <h1 className="content__title">Login</h1>
             </header>
 
-            <div class="content__posts">
-
+            <div className="content__posts">
+                <form className='form-login' onSubmit={loginUser}>
+                    <div className="form-group">
+                        <label htmlFor='email'>Email</label>
+                        <input type='email' name='email' onChange={changed} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor='password'>Constraseña</label>
+                        <input type='password' name='password' onChange={changed} />
+                    </div>
+                    <input type="submit" value='Identificate' className='btn btn-success' />
+                </form>
 
             </div>
         </>
