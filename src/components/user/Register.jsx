e66bdc8fe00d13aef3edf5a35aa34ee00e95edcf
Andrés Darín <andres.darin05@gmail.com>
2024-08-07 09:30:24 -0300

    useForm | Register

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -1,35 +1,48 @@
 import React from 'react'
+import { useForm } from '../../hooks/useForm'
 
 export const Register = () => {
+
+    const { form, changed } = useForm({})
+    const saveUser = (e) => {
+        e.preventDefault();
+        let newUser = form;
+
+        console.log(newUser);
+    }
+
     return (
         <>
-            <header class="content__header content__header--public">
-                <h1 class="content__title">Registro</h1>
+            <header className="content__header content__header--public">
+                <h1 className="content__title">Registro</h1>
             </header>
 
-            <div class="content__posts">
-                <form className='register-form'>
+            <div className="content__posts">
+
+                <form className='register-form' onSubmit={saveUser}>
+
                     <div className="form-group">
                         <label htmlFor="name">Nombre</label>
-                        <input type="text" name='name' />
+                        <input type="text" name='name' onChange={changed} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="surname">Apellido</label>
-                        <input type="text" name='surname' />
+                        <input type="text" name='surname' onChange={changed} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="nick">Nick</label>
-                        <input type="text" name='nick' />
+                        <input type="text" name='nick' onChange={changed} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="email">eMail</label>
-                        <input type="text" name='email' />
+                        <input type="email" name='email' onChange={changed} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Contraseña</label>
-                        <input type="text" name='password' />
+                        <input type="password" name='password' onChange={changed} />
                     </div>
                     <input type='submit' value='Registrate' className='btn btn-success'></input>
+
                 </form>
 
             </div>
