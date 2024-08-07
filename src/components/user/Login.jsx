85d8b4c15361136b670c099f7ea9cc718c20a58f
Andrés Darín <andres.darin05@gmail.com>
2024-08-07 11:16:04 -0300

    User Identification

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from '../../hooks/useForm'
+import { Global } from '../../helpers/Global'
 
 export const Login = () => {
 
     const { form, changed } = useForm({})
+    const [loged, setLoged] = useState("not_sended")
     const loginUser = async (e) => {
         e.preventDefault();
 
@@ -11,8 +13,28 @@ export const Login = () => {
         let userToLogin = form;
 
         //Peticion al backend 
+        const request = await fetch(Global.url + 'user/login', {
+            method: 'POST',
+            body: JSON.stringify(userToLogin),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
 
-        //Persistir los datos en el navegador
+        const data = await request.json();
+
+
+        if (data.status === 'success') {
+
+            //Persistir los datos en el navegador
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('user', JSON.stringify(data.user));
+
+
+            setLoged('loged');
+        } else {
+            setLoged('error')
+        }
 
     }
 
@@ -23,6 +45,8 @@ export const Login = () => {
             </header>
 
             <div className="content__posts">
+                {loged == 'loged' ? <strong className='alert alert-success'>Ususario Identificado Correctamente </strong> : ''}
+                {loged == 'error' ? <strong className='alert alert-danger'>Ususario no Identificado </strong> : ''}
                 <form className='form-login' onSubmit={loginUser}>
                     <div className="form-group">
                         <label htmlFor='email'>Email</label>
