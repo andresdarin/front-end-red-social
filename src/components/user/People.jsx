9fd26be9d3a26514b2bb7720286f55e643fa1ab8
Andrés Darín <andres.darin05@gmail.com>
2024-08-14 18:56:54 -0300

    Follow | Unfollow

--- src/components/user/People.jsx
+++ src/components/user/People.jsx
@@ -6,6 +6,7 @@ export const People = () => {
     const [users, setUsers] = useState([]);
     const [page, setPage] = useState(1);
     const [more, setMore] = useState(true)
+    const [following, setFollowing] = useState([])
     const [loading, setLoading] = useState(false); // Estado para manejar carga
 
     const token = localStorage.getItem('token');
@@ -34,6 +35,7 @@ export const People = () => {
             if (data.users && data.status === 'success') {
                 let newUsers = [...users, ...data.users]; // Agrega nuevos usuarios a la lista existente
                 setUsers(newUsers);
+                setFollowing(data.user_following)
             }
 
             if (users.length >= (data.total - data.users.length)) {
@@ -48,6 +50,51 @@ export const People = () => {
 
     const nextPage = () => {
         setPage(prevPage => prevPage + 1); // Incrementa la página
+
+    }
+
+    const follow = async (userId) => {
+        //Peticion al backend para guardar el follow
+        const request = await fetch(Global.url + 'follow/save', {
+            method: 'POST',
+            body: JSON.stringify({ followed: userId }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token
+            }
+        })
+
+        const data = await request.json();
+
+        //Cuando esté todo correcto
+        if (data.status == 'success') {
+            //Actualizar estado del following agregando el nuevo follow
+            setFollowing([...following, userId])
+        }
+
+
+    }
+
+    const unfollow = async (userId) => {
+        //Peticion al backend para borrar el follow
+        const request = await fetch(Global.url + 'follow/unfollow/' + userId, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token
+            }
+        })
+
+        const data = await request.json();
+
+        //Cuando esté todo correcto
+        if (data.status == 'success') {
+            //Actualizar estado del following filtrando los datos para eliminar el antiguo user id que acabo de dejar de seguir
+            let filterFollowings = following.filter(followingUserId => userId !== followingUserId)
+            setFollowing(filterFollowings)
+        }
+
+
     }
 
     return (
@@ -57,6 +104,7 @@ export const People = () => {
             </header>
 
             <div className="content__posts">
+
                 {users.map(user => (
                     <article className="posts__post" key={user._id}>
                         <div className="post__container">
@@ -77,9 +125,17 @@ export const People = () => {
                             </div>
                         </div>
 
-                        <div className="post__buttons">
-                            <a href="#" className="post__button post__button-green">Seguir</a>
-                        </div>
+                        {!following.includes(user._id) &&
+                            <button className="post__button post__button-green"
+                                onClick={() => follow(user._id)}>
+                                Seguir
+                            </button>}
+                        {following.includes(user._id) &&
+                            <button className='post__button'
+                                onClick={() => unfollow(user._id)}>
+                                Dejar de Seguir
+                            </button>}
+
                     </article>
                 ))}
             </div>
